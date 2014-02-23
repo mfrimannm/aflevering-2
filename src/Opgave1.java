3f14ccecad31cd39c4ae1f2e47c2499cf2783cc7
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-02-23 01:03:58 +0100

    begyndt på programmet
    har skrevet en metode som tjekker for antal 1000 og returener en streng
    med det antal M'er
    dog skal det laves om så den bare retunrer et tal og vi i metoden
    konverterNummer lavet strengen, så kan vi komme uden om at sende flere
    værdier frem og tilbage mellem metoderne.

--- src/Opgave1.java
+++ src/Opgave1.java
@@ -3,7 +3,27 @@ public class Opgave1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String romer = konverterNummer(1000);
+		System.out.print(romer);
+		
+	}
 
+	private static String konverterNummer(int i) {
+		// TODO Auto-generated method stub
+		String s = tjekTusind(i);
+		return s;
+	}
+
+	private static String tjekTusind(int i) {
+		// TODO Auto-generated method stub
+		int Antal = i / 1000;
+		System.out.println(Antal);
+		
+		String s = "h";
+		for(int k=1;k<=Antal;k++){
+			s = s + "M";
+		}
+		return s;
 	}
 
 }
