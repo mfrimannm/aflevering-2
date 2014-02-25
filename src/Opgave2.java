8048105a4c046d29179ca9bf2fc038ef0ccf8d03
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-02-25 12:54:33 +0100

    lavet opgave 2
    Har lavet den ved hjælp af StringBuilder(input).reverse().toString(); og
    input.replaceAll("\\W", "");

--- src/Opgave2.java
+++ src/Opgave2.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 
 public class Opgave2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		tjekForPalindrome();
+	}
 
+	private static void tjekForPalindrome() {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter line to check: ");
+		String input = scan.next();
+		System.out.println(input);
+		
+		// fjerner specil tegn
+		input = input.replaceAll("\\W", "");
+
+		// tjek
+		String omvent = new StringBuilder(input).reverse().toString();
+		Boolean jaEllerNej = omvent.equalsIgnoreCase(input);
+		// print
+		if (jaEllerNej) {
+			System.out.print("\"" + input + "\"" + " is a palindrome!");
+		} else {
+			System.out.print("\"" + input + "\"" + " is not a palindrome!");
+		}
+		scan.close();
 	}
 
 }
