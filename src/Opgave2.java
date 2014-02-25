86aebe9738c12c94e78974ee18b1ef2332ddbe09
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-02-25 13:05:00 +0100

    rettet next til nextine
    og tjekker for om inputtet bliver tomt når man fjerner speciltegn

--- src/Opgave2.java
+++ src/Opgave2.java
@@ -11,12 +11,16 @@ public class Opgave2 {
 		Scanner scan = new Scanner(System.in);
 
 		System.out.print("Enter line to check: ");
-		String input = scan.next();
+		String input = scan.nextLine();
 		System.out.println(input);
-		
+
 		// fjerner specil tegn
 		input = input.replaceAll("\\W", "");
 
+		if (input.length() <= 0) {
+			scan.close();
+			throw new IllegalArgumentException("input er tom, dette kan være fordi den med special tegn fjernet er blevet tom");
+		}
 		// tjek
 		String omvent = new StringBuilder(input).reverse().toString();
 		Boolean jaEllerNej = omvent.equalsIgnoreCase(input);
