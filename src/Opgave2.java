2ba7a64ef2fdd8d6f37f8f39493ba05f0f37a5f6
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-02-27 23:09:24 +0100

    har skrevet kommentare ind

--- src/Opgave2.java
+++ src/Opgave2.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Opgave2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Man kalder metoden
 		tjekForPalindrome();
 	}
 
 	private static void tjekForPalindrome() {
+//		når den er kaldt vil den sprøge til en string og printe resultatet.
 		Scanner scan = new Scanner(System.in);
 
 		System.out.print("Enter line to check: ");
@@ -22,6 +23,8 @@ public class Opgave2 {
 			throw new IllegalArgumentException("input er tom, dette kan være fordi den med special tegn fjernet er blevet tom");
 		}
 		// tjek
+		// vi har i class'en StringBuilder metoder til at omvenne og lave kompelkse ting meget nemt.
+		// denne bygger en string ud fra indputet og arbejder så med denne til sidst laver den så dette tilbage om til en string.
 		String omvent = new StringBuilder(input).reverse().toString();
 		Boolean jaEllerNej = omvent.equalsIgnoreCase(input);
 		// print
