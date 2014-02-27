237ac8c0adb72b0899f83073a9b1ad01e4e0119f
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-02-27 23:10:39 +0100

    skrevet kommentare og fjernet debug console print

--- src/Opgave3.java
+++ src/Opgave3.java
@@ -3,61 +3,80 @@ import java.util.Scanner;
 public class Opgave3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Den laver et metode kald som giver en et sprøgsmål om antal
+		// iterationer. den vil retunerer en double med resultetet.
 		double nearPi = antalKastMedBuffonsNeedle();
 		System.out.println("tilnærmere værdi af Pi " + nearPi);
 	}
 
 	private static double antalKastMedBuffonsNeedle() {
-		// TODO Auto-generated method stub
+		// den bygger en scanner som så sprøg til antal iterationer som at man
+		// vil undersøge.
 		System.out.print("hvor mange kast vil du foretage? ");
 		Scanner scan = new Scanner(System.in);
 		int antalKast = scan.nextInt();
+		// vi giver ikke forsætte hvis vi kun skulle gøre det under 1 gang.
 		if (antalKast < 1) {
 			scan.close();
 			throw new IllegalArgumentException("Ingen kast bedt om");
 		}
+		// men ellers så sender vi antal iterationer til den metode som beregner
+		// Pi.
 		double nearPi = buffonsNeedle(antalKast);
-
+		// vi får så Pi og sender denne tilbage til main.
 		scan.close();
 		return nearPi;
 	}
 
 	private static double buffonsNeedle(int antalKast) {
-		// TODO Auto-generated method stub
+		// denne berenger Pi, først bestemes nogen konsanter som faktsik ikke er
+		// så afgørende bare deres forhold er rigtig.
+		// dette vil de altså være efter som den ene er lig den anden med en
+		// koifient.
 		int lengthOfStick = 1;
 		int lengthBetweenLines = lengthOfStick * 2;
 
-		double nearPi = -1;
+		// da jeg skal bruge denne i loopet skal den lige difineres.
 		int hits = 0;
 
 		for (int i = 1; i <= antalKast; i++) {
+			// bestemer så nogen tilfældige værdier.
 			double a = Math.random() * lengthBetweenLines;
 			double b = Math.random() * 180;
 			double restLength = 0;
+			// og tjekker så hvis vi har nogen specil tilfælde som giver nemmere
+			// beregninger om vi kan køre disse bare.
 			if (a == 2 || a == 0) {
 				hits++;
-				System.out.print("var her");
+//				System.out.print("var her");
 			} else {
 				if (b == 180 || b == 0) {
 					restLength = lengthOfStick;
 				} else {
-					// reng med cos og sin
+					// renger med sin
+					// vi skal lige bruge den rigtige vinkel som ikke er den som
+					// er gennerretet men findes ud fra denne
+					// jeg bruger sinus relleration til at finde længden hvorfor
+					// jeg skal bruge den anden vinkel.
 					b = 180 - 90 - b;
 					restLength = (Math.sin(Math.toRadians(b)) * lengthOfStick)
 							/ (Math.sin(Math.toRadians(90)));
 				}
 				// brug længderne
-			if ((a + restLength) >= lengthBetweenLines  || (a + restLength) <= 0) {
+				// her tjekker jeg bare længderne og hvis det passer sådan at
+				// den er mere end 2 så må vi have et hit.
+				// eller den er under 0, hvilke bare ville betyde en forskydning, at den lå i det næste mellemrum.
+				if ((a + restLength) >= lengthBetweenLines
+						|| (a + restLength) <= 0) {
 					hits++;
 				}
 			}
 		}
-		System.out.println(hits);
+//		System.out.println(hits);
 		if (hits == 0) {
 			throw new IllegalArgumentException("Ingen hits");
 		}
-		nearPi = antalKast / (double) hits;
+		double nearPi = antalKast / (double) hits;
 		return nearPi;
 	}
 }
